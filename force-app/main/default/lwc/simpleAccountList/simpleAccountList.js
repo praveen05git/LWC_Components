0184ce219fdde5078ffb7b9827864170fb147a8e
praveen05git <praveenmail@protonmail.com>
2023-04-06 18:26:56 +0530

    Project commit

---
+++ force-app/main/default/lwc/simpleAccountList/simpleAccountList.js
@@ -0,0 +1,50 @@
+import { LightningElement, wire, track } from 'lwc';
+import getAccounts from '@salesforce/apex/AccountsController.getAllAccounts';
+
+export default class SimpleAccountList extends LightningElement {
+    accountData;
+    @track listViewColumns = [
+        {
+            label: 'Account Name',
+            fieldName: 'Name',
+            type: 'text'
+        },
+        {
+            label: 'Industry',
+            fieldName: 'Industry',
+            type: 'text'
+        },
+        {
+            label: 'Annual Revenue',
+            fieldName: 'AnnualRevenue',
+            type: 'currency'
+        },
+        {
+            label: 'No. of Employees',
+            fieldName: 'NumberOfEmployees',
+            type: 'text'
+        },
+        {
+            label: 'Phone',
+            fieldName: 'Phone',
+            type: 'text'
+        }
+    ];
+
+    @wire(getAccounts)
+    allAccounts({ error, data }) {
+        if (data) {
+            /*
+            let nameUrl;
+            this.accountData = data.map(row => {
+                nameUrl = this.baseUrl + 'account' + `/${row.Id}`;
+                return { ...row, nameUrl };
+            });
+            this.dataForList = this.accountData;
+            */
+            this.accountData = data;
+        } else if (error) {
+            console.log('Table Error: ' + error.body.message);
+        }
+    }
+}
